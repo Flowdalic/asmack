02e17d43eb3951df2bd30d73dc6b122d24043d0a
Florian Schmaus <fschmaus@gmail.com>
2012-06-04 23:33:58 +0200

    Added new SmackAndroid class, droping jse support. Fixes #5

---
+++ static-src/custom/org/jivesoftware/smack/SmackAndroid.java
@@ -0,0 +1,34 @@
+package com.googlecode.gtalksms;
+
+import org.jivesoftware.smackx.ConfigureProviderManager;
+import org.xbill.DNS.ResolverConfig;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+public class SmackAndroid {
+    private static SmackAndroid sSmackAndroid = null;
+    
+    private SmackAndroid(Context ctx) {
+        ConfigureProviderManager.configureProviderManager();
+        ctx.registerReceiver(new ConnectivtyChangedReciever(), new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
+    }
+    
+    public static SmackAndroid init(Context ctx) {
+        if (sSmackAndroid == null) {
+            sSmackAndroid = new SmackAndroid(ctx);
+        }
+        return sSmackAndroid;
+    }
+    
+    class ConnectivtyChangedReciever extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ResolverConfig.refresh();
+        }
+        
+    }
+}
