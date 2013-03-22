18a81494e8f2752740f2e450cb2b024aeafd3bc5
Leo Accend <developer.aeonic@xoxy.net>
2013-03-22 08:36:55 -0700

    GitHub Pull Request #29: Adding .hg to the list of excluded directories.

--- build.bash
+++ build.bash
@@ -145,7 +145,7 @@ copyfolder() {
   cd ${ASMACK_BASE}
   (
     cd "${1}"
-    tar -cp --exclude=.svn --exclude=.git "${3}"
+    tar -cp --exclude=.svn --exclude=.git --exclude=.hg "${3}"
   ) | (
     cd "${2}"
     tar -xp
