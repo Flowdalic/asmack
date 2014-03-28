f766a60dfd964397cb215cefdc407838c1eaa59f
Florian Schmaus <flo@geekplace.eu>
2014-03-28 11:09:11 +0100

    Update SmackAndroid
    the maybeRegisterMethod is required to ensure that the ConnectivityChange listener is active.

--- static-src/custom/org/jivesoftware/smack/SmackAndroid.java
+++ static-src/custom/org/jivesoftware/smack/SmackAndroid.java
@@ -8,47 +8,55 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 
 public class SmackAndroid {
-    private static SmackAndroid sSmackAndroid = null;
+	private static SmackAndroid sSmackAndroid = null;
 
 	private BroadcastReceiver mConnectivityChangedReceiver = new BroadcastReceiver() {
-			@Override
-			public void onReceive(Context context, Intent intent) {
-				ResolverConfig.refresh();
-			}
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			ResolverConfig.refresh();
+		}
 	};
 
 	private static boolean receiverRegistered = false;
 	private Context mCtx;
 
-    private SmackAndroid(Context ctx) {
-        mCtx = ctx;
-        DNSUtil.setDNSResolver(DNSJavaResolver.getInstance());
-		mCtx.registerReceiver(mConnectivityChangedReceiver, new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
-		receiverRegistered = true;
-    }
+	private SmackAndroid(Context ctx) {
+		mCtx = ctx;
+		DNSUtil.setDNSResolver(DNSJavaResolver.getInstance());
+	}
 
 	/**
 	 * Init Smack for Android. Make sure to call
 	 * SmackAndroid.onDestroy() in all the exit code paths of your
 	 * application.
 	 */
-    public static synchronized SmackAndroid init(Context ctx) {
-        if (sSmackAndroid == null) {
-            sSmackAndroid = new SmackAndroid(ctx);
-        }
-        return sSmackAndroid;
-    }
+	public static synchronized SmackAndroid init(Context ctx) {
+		if (sSmackAndroid == null) {
+			sSmackAndroid = new SmackAndroid(ctx);
+		}
+		sSmackAndroid.maybeRegisterReceiver();
+		return sSmackAndroid;
+	}
 
 	/**
 	 * Cleanup all components initialized by init(). Make sure to call
 	 * this method in all the exit code paths of your application.
 	 */
-    public synchronized void onDestroy() {
+	public synchronized void onDestroy() {
 		if (receiverRegistered) {
 			mCtx.unregisterReceiver(mConnectivityChangedReceiver);
 			receiverRegistered = false;
 		}
-    }
+	}
+
+	private void maybeRegisterReceiver() {
+		if (!receiverRegistered) {
+			mCtx.registerReceiver(mConnectivityChangedReceiver, new IntentFilter(
+					ConnectivityManager.CONNECTIVITY_ACTION));
+			receiverRegistered = true;
+		}
+	}
 }
